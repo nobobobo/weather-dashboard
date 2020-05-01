6b586907c9ca52eac1b416a6003afadf8a2528ee
nobobobo <ianhsu1221@gmail.com>
2020-04-30 21:35:31 -0700

    added a function to render 5-day forecast

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function renderCity() {
         liElm.click(function () {
             city = $(this).attr("id");
             renderForecast();
+            renderFiveDay();
         })
         $("#cityList").append(liElm);
     }
@@ -24,7 +25,7 @@ function renderForecast() {
         method: "GET"
     }).then(function (forecastData) {
         let date = new Date(forecastData.dt * 1000);
-        let dateFormatted = " (" + date.getMonth() + "/" + date.getDay() + ", " + date.getFullYear() + ")";
+        let dateFormatted = " (" + (parseInt(date.getMonth())+1) + "/" + date.getDate() + ", " + date.getFullYear() + ")";
         let iconUrl = "http://openweathermap.org/img/wn/" + forecastData.weather[0].icon + "@2x.png";
 
         $("#forecast").append($("<h1>").text(forecastData.name + dateFormatted).append($("<img>").attr("src", iconUrl)));
@@ -43,14 +44,53 @@ function renderForecast() {
     $("#forecastContainer").attr("style", "display:block;")
 }
 
-renderCity();
-renderForecast();
+function renderFiveDay(){
+    $("#fiveDayForecast").empty();
+
+    let fiveDayUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=5cea1d9c8d9729e3deba8cb0ccf8f1d8"
+    $("#fiveDayForecast").append($("<h2>").text("5-Day Forecast").addClass("m-3"));
+    $.ajax({
+        url:fiveDayUrl,
+        method: "GET"
+    }).then(function (forecastData){
+        console.log(forecastData);
+    
+        for (let i=0; i<5; i++){
+            let cardElm = $("<div>").addClass("weather-card flex-fill");
+            let cardData = $("<div>").addClass("border text-white bg-primary rounded m-3");
+
+            let date = new Date(forecastData.list[8*i+7].dt * 1000);
+            console.log(date);
+            let dateFormatted = (parseInt(date.getMonth())+1) + "/" + date.getDate() + ", " + date.getFullYear();
+
+            
+            cardData.append($("<h2>").text(dateFormatted).addClass("m-1  text-center"));
+
+            let iconUrl = "http://openweathermap.org/img/wn/" + forecastData.list[8*i+7].weather[0].icon + "@2x.png";
+
+            cardData.append($("<img>").attr("src", iconUrl).attr("style","width:25%;")).addClass(" text-center");
+
+            cardData.append($("<p>").addClass("m-1 text-center").text("Temp: " + forecastData.list[8*i+7].main.temp + " °F"));
+            cardData.append($("<p>").addClass("m-1 text-center").text("Humidity: " + forecastData.list[8*i+7].main.humidity + " %"));
+
+
+
+            cardElm.append(cardData);
+            $("#fiveDayForecast").append(cardElm);
+        }
+        
+    })
+
+    $("#fiveDayForecast").attr("style", "display:flex;")
+}
+
 
 $("#search").click(function () {
     city = $("#input").val();
     cityList.push(city);
     renderCity();
     renderForecast();
+    renderFiveDay();
 })
 
 $("#input").keypress(function (event) {
@@ -60,5 +100,6 @@ $("#input").keypress(function (event) {
         cityList.push(city);
         renderCity();
         renderForecast();
+        renderFiveDay();
     }
 })
